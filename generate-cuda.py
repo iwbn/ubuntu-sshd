c8ff0b627f4af3e648473a2c132d733efade3b2a
unknown <iwbn@kaist.ac.kr>
2020-12-14 20:31:03 +0900

    generate wanted image

---
+++ generate-cuda.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+
+IMAGES = [
+	'nvidia/cuda:10.1-cudnn7-devel-ubuntu18.04',
+	'nvidia/cuda:10.1-cudnn7-devel-ubuntu16.04',
+	'nvidia/cuda:10.2-cudnn7-devel-ubuntu16.04',
+	'nvidia/cuda:10.2-cudnn7-devel-ubuntu18.04',
+	'nvidia/cuda:10.2-cudnn8-devel-ubuntu16.04',
+	'nvidia/cuda:10.2-cudnn8-devel-ubuntu18.04',
+	'nvidia/cuda:11.0-cudnn8-devel-ubuntu18.04',
+	'nvidia/cuda:11.1-cudnn8-devel-ubuntu18.04',
+	'tensorflow/tensorflow:nightly-gpu',
+	'tensorflow/tensorflow:latest-gpu',
+	'pytorch/pytorch:1.7.0-cuda11.0-cudnn8-devel',
+	'pytorch/pytorch:1.6.0-cuda10.1-cudnn7-devel',
+	'pytorch/pytorch:1.5.1-cuda10.1-cudnn7-devel'
+]
+
+for image in IMAGES:
+	subprocess.run(['python3 generate.py ' % image], shell=True, check=True)
+
--- generate.py
+++ generate.py
@@ -1,23 +1,30 @@
 import os
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(description='make ssh-enabled docker image from base image')
+parser.add_argument('base_image', type=str, help='base image name')
+args = parser.parse_args()
+
 
 with open("Dockerfile.template", 'r') as f:
 	template = f.read()
 
-CUDA_CUDNN_VERSION = [("10.1", "7"), ("10.2", "7"), ("10.2","8"), ("11.0", "8")]
-UBUNTU_VERSION = ["18.04"]
+
+res = template.replace("%BASE-DOCKER%", args.base_image)
 
 
-for ubuntu_version in UBUNTU_VERSION:
-	for cuda_version, cudnn_version in CUDA_CUDNN_VERSION:
-		res = template.replace("%CUDA_VERSION%", cuda_version)
-		res = res.replace("%UBUNTU_VERSION%", ubuntu_version)
-		res = res.replace("%CUDNN_VERSION%", cudnn_version)
-		
-		dir_name = '%s-%s-%s' % (ubuntu_version, cuda_version, cudnn_version)
+name = args.base_image.split("/")[-1]
+base_name = name.split(":")[0]
+if len(args.base_image.split(":")) > 1:
+	raise ValueError("plase give base_image name with tag")
+base_tag = args.base_image.split(":")[1]
+new_name = base_name + "-sshd" + ":" + base_tag
 
-		os.makedirs(dir_name, exist_ok=True)
-		with open(os.path.join(dir_name, 'Dockerfile'), 'w') as f:
-			f.write(res)
-		subprocess.run(['docker build -t %s .' % "sgvr-gpu-base:2.0-cuda%s-cudnn%s-devel-ubuntu%s" % (cuda_version, cudnn_version, ubuntu_version)], shell=True, check=True, cwd=dir_name)
+dir_name = new_name
+
+os.makedirs(dir_name, exist_ok=True)
+with open(os.path.join(dir_name, 'Dockerfile'), 'w') as f:
+	f.write(res)
+subprocess.run(['docker build -t %s .' % new_name], shell=True, check=True, cwd=dir_name)
 
